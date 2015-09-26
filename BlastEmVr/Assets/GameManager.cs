e27f5de04b1510db9f7c9c566e12f3b88f0f9f3b
Ngoc Do <ngocmdo@gmail.com>
2015-09-26 16:37:55 -0700

    add ball placement

--- BlastEmVr/Assets/GameManager.cs
+++ BlastEmVr/Assets/GameManager.cs
@@ -1,20 +1,34 @@
 ﻿using System;
 using UnityEngine;
 using System.Collections;
+using UnityEditor;
 
-public class GameManager : MonoBehaviour {
+public class GameManager : MonoBehaviour
+{
+
+    private GameObject _cannonBall;
 
 	// Use this for initialization
-	void Start () {
+	void Start ()
+	{
+        _cannonBall = GameObject.FindGameObjectWithTag("CannonBall");
+        _cannonBall.SetActive(false);
+
 	    Cardboard.SDK.OnTrigger += delegate
 	    {
-	        Quaternion rotation = Cardboard.SDK.HeadPose.Orientation;
-	        Debug.Log("x: " + rotation.x + "");
-            Debug.Log("y: " + rotation.y + "");
 	    };
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	}
+
+    private void ShootCannonBall()
+    {
+        Quaternion rotation = Cardboard.SDK.HeadPose.Orientation;
+        Debug.Log("x: " + rotation.x + "");
+        Debug.Log("y: " + rotation.y + "");
+
+        _cannonBall.SetActive(true);
+    }
 }
