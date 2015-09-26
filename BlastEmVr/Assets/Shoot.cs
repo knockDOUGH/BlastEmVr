bd41c4deb968c498018fc137d05e3a4255535f0d
Ngoc Do <ngocmdo@gmail.com>
2015-09-26 14:13:31 -0700

    add tank, attach camera to tank tower

---
+++ BlastEmVr/Assets/Shoot.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shoot : MonoBehaviour
+{
+
+    private GazeInputModule _input;
+
+	// Use this for initialization
+	void Start () {
+	    _input = new GazeInputModule();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
