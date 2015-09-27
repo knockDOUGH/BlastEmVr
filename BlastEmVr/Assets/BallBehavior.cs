4951acebc97a760d03db1a027a5d115274c50209
Oak Nguyen <oak@bizible.com>
2015-09-26 18:01:33 -0700

    blaj

---
+++ BlastEmVr/Assets/BallBehavior.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallBehavior : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnCollisionEnter(Collision c)
+    {
+        // destroy the rocket
+        // note:
+        //  Destroy(this) would just destroy the rocket
+        //                script attached to it
+        //  Destroy(gameObject) destroys the whole thing
+        Destroy(gameObject);
+    }
+}
--- BlastEmVr/Assets/GameManager.cs
+++ BlastEmVr/Assets/GameManager.cs
@@ -5,30 +5,92 @@ using UnityEditor;
 
 public class GameManager : MonoBehaviour
 {
+    public GameObject _cannonBall;
+    public GameObject _tank;
+    public GameObject _cannonBallPlaceholder;
 
-    private GameObject _cannonBall;
+    public float maxPower;
+    public float powerIncreaseRate;
 
-	// Use this for initialization
-	void Start ()
+    private bool pulling = false;
+    private float power = 0;
+
+    // Use this for initialization
+    void Start ()
 	{
-        _cannonBall = GameObject.FindGameObjectWithTag("CannonBall");
-        _cannonBall.SetActive(false);
+        //_cannonBall = GameObject.FindGameObjectWithTag("CannonBall");
+        //_cannonBall.SetActive(false);
 
 	    Cardboard.SDK.OnTrigger += delegate
 	    {
 	    };
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        // left mouse clicked?
+        if (Input.GetMouseButtonDown(0))
+        {
+            this.startPull();
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            this.stopPull();
+        }
+
+        if (pulling)
+        {
+            power += powerIncreaseRate;
+
+            if (power > maxPower) power = maxPower;
+        }
+    }
+
+
+    void OnGUI()
+    {
+        GUI.Box(new Rect(0, 0, 100, 50), this.power.ToString());
+    }
+
+
+    private void startPull()
+    {
+        this.pulling = true;
+        this.power = 0;
+    }
+
+    private void stopPull()
+    {
+        this.pulling = false;
+        this.ShootCannonBall();
+    }
 
     private void ShootCannonBall()
     {
-        Quaternion rotation = Cardboard.SDK.HeadPose.Orientation;
-        Debug.Log("x: " + rotation.x + "");
-        Debug.Log("y: " + rotation.y + "");
+        Vector3 position = _cannonBallPlaceholder.transform.position;
+        Quaternion rotation = _cannonBallPlaceholder.transform.rotation;
 
-        _cannonBall.SetActive(true);
+        GameObject ball = this.spawnBall(position, rotation);
+        //bear.GetComponent<Rigidbody>().AddRelativeForce(position * this.power);
+
+    }
+
+    private GameObject spawnBall(Vector3 position, Quaternion rotation)
+    {
+        // spawn rocket
+        // - Instantiate means 'throw the prefab into the game world'
+        // - (GameObject) cast is required because unity is stupid
+        // - transform.position because we want to instantiate it exactly
+        //   where the weapon is
+        // - transform.parent.rotation because the rocket should face the
+        //   same direction that the player faces (which is the weapon's
+        //   parent.
+        //   we can't just use the weapon's rotation because the weapon is
+        //   always rotated like 45° which would make the bullet fly really
+        //   weird
+        GameObject g = (GameObject)Instantiate(_cannonBall, position, rotation);
+        return g;
     }
 }
